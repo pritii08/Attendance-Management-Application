9e42386da72048e80393db7578563f5f6c402cda
pritii08 <pritimule08@gmail.com>
2024-01-20 20:19:48 +0530

    First file from local

---
+++ Main.java
@@ -0,0 +1,11 @@
+import javax.swing.JFrame;
+
+public class Main extends JFrame{
+    public Main(){
+            setVisible(true);
+            
+    }
+    public static void main(String[] args) {
+        new Main();
+    }
+}
